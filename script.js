e1056f82707fc1df545f3af2fb3de98a8a439806
Ben Grabowski <gbowski30@gmail.com>
2019-05-30 20:16:10 -0600

    finished API functionality

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const baseURL = 'https://developer.nps.gov/api/v1/parks';
+const apiKey = 'yLjjGMohTz9qmXzu7KeYtzvBqrWB45Cwb5VYvUKY';
+
+function getParks(states, maxResults){
+    const params = {
+        api_key: apiKey,
+        stateCode: states,
+        limit: maxResults
+    };
+
+    const queryString = formatParams(params);
+    const url = baseURL + '?' + queryString;
+
+    console.log(url);
+    console.log('getParks ran');
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+        $('#error-message').text(`Something went wrong: ${error.message} `);
+    });
+}
+
+function displayResults(responseJson){
+    $('#results-list').empty();
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $('#results-list').append(
+            `<li>
+                <h3>${responseJson.data[i].fullName}</h3>
+                <p>${responseJson.data[i].description}</p>
+                <a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a>
+                <p>${responseJson.data[i].addresses[0].line1}</p>
+                <p>${responseJson.data[i].addresses[0].line2}</p>
+                <p>${responseJson.data[i].addresses[0].line3}</p>
+                <p>${responseJson.data[i].addresses[0].city}</p>
+                <p>${responseJson.data[i].addresses[0].stateCode}</p>
+                <p>${responseJson.data[i].addresses[0].postalCode}</p>
+            </li>`
+        )}
+    $('#results').removeClass('hidden');
+}
+
+function formatParams(params){
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function handleSubmit(){
+    $('#submit').on('click', event => {
+        event.preventDefault();
+        const states = $('input[name=states]').val();
+        const maxResults = $('input[name=max-results]').val() - 1;
+        getParks(states, maxResults);
+    });
+    
+}
+
+$(handleSubmit);
